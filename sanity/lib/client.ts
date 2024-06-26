e0aaaf24e33a51b738fff1477e271687f2d57682
hyunsunglimm <gustjd3834@naver.com>
2024-06-26 16:43:43 +0900

    Chore: sanity api token setting

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,13 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn,
-  perspective: 'published',
-})
+  perspective: "published",
+  token: process.env.SANITY_SECRET_TOKEN,
+  ignoreBrowserTokenWarning: true,
+});
