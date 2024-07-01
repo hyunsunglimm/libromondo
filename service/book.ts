c6b975f3fd481303b9421a131774f4066042020b
hyunsunglimm <gustjd3834@naver.com>
2024-07-01 21:16:38 +0900

    Feat: Yes24 simple web Crawling

--- service/book.ts
+++ service/book.ts
@@ -1,13 +1,26 @@
-export const getTodayBooks = async () => {
-  const res = await fetch(
-    "https://dapi.kakao.com/v3/search/book?query=주술회전&size=1",
-    {
-      headers: {
-        Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
-      },
-    }
-  );
-  const data = await res.json();
+import { BookResponseType } from "@/app/api/book/type";
+import axios from "axios";
 
+const getBestBooks = async () => {
+  const res = await fetch("http://localhost:3000/api/crawler");
+  const data = await res.json();
   return data;
+};
+
+const fetchBookData = async (title: string) => {
+  return fetch(`https://dapi.kakao.com/v3/search/book?query=${title}&size=1`, {
+    headers: {
+      Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+    },
+  }).then((res) => res.json());
+};
+
+export const getTodayBooks = async (): Promise<BookResponseType[]> => {
+  const bestBookTitles = await getBestBooks();
+
+  const data = await Promise.all(bestBookTitles.map(fetchBookData));
+
+  const bookData = data.map((d) => d.documents[0]);
+
+  return bookData;
 };
