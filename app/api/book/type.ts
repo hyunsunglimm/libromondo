d0a25f0841d29830c66cd028f1de834de575e148
hyunsunglimm <gustjd3834@naver.com>
2024-07-01 21:19:12 +0900

    Type: BookResponseType

---
+++ app/api/book/type.ts
@@ -0,0 +1,14 @@
+export type BookResponseType = {
+  authors: string[];
+  contents: string;
+  datetime: string;
+  isbn: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: string;
+  thumbnail: string;
+  title: string;
+  translators: string[];
+  url: string;
+};
