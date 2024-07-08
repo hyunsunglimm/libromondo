2da5d1443f3e66a4932032d44cbbecd473578556
hyunsunglimm <gustjd3834@naver.com>
2024-07-08 17:34:12 +0900

    Chore: add not found page

--- app/(DefaultLayout)/layout.tsx
+++ app/(DefaultLayout)/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Inter } from "next/font/google";
-import "../globals.css";
+
 import SWRConfigContext from "@/context/SWRConfigContext";
 import AuthContext from "@/context/AuthContext";
 
@@ -11,26 +10,20 @@ export const metadata: Metadata = {
   description: "Libro Mondo는 간편한 도서 추천 서비스입니다.",
 };
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function DefaultLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <AuthContext>
-          <SWRConfigContext>
-            <Header />
-            <main className="grow flex max-w-[1280px] mx-auto w-full py-12">
-              {children}
-            </main>
-            <Footer />
-          </SWRConfigContext>
-        </AuthContext>
-      </body>
-    </html>
+    <AuthContext>
+      <SWRConfigContext>
+        <Header />
+        <main className="grow flex max-w-[1280px] mx-auto w-full py-12">
+          {children}
+        </main>
+        <Footer />
+      </SWRConfigContext>
+    </AuthContext>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
-export default function EmptyLayout({
+import "./globals.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        {children}
+      </body>
     </html>
   );
 }
