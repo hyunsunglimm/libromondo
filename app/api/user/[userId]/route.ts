c8eb9a2298776d958b44836fce1405ab7eb7f3ab
hyunsunglimm <gustjd3834@naver.com>
2024-07-08 21:27:23 +0900

    Refactor: validating cached user data

--- app/api/user/[userId]/route.ts
+++ app/api/user/[userId]/route.ts
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { addSave, getUserById, removeSave } from "@/service/user";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 type Context = {
   params: {
