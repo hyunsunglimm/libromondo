b06d15d4ec5a4c4db311476bb018921fc1d0f362
hyunsunglimm <gustjd3834@naver.com>
2024-07-16 20:52:02 +0900

    Fix: bulid image issue 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,6 @@
 import "./globals.css";
 
 import { Inter } from "next/font/google";
-// import type { Viewport } from "next";
-
-// export const viewport: Viewport = {
-//   width: "device-width",
-//   initialScale: 1,
-//   maximumScale: 1,
-//   userScalable: false,
-// };
 
 const inter = Inter({ subsets: ["latin"] });
 
