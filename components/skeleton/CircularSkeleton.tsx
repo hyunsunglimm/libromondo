414eba3a516340f72e685c6a616780036341d593
hyunsunglimm <gustjd3834@naver.com>
2024-07-16 14:06:47 +0900

    Style: circular skeleton fix

--- components/skeleton/CircularSkeleton.tsx
+++ components/skeleton/CircularSkeleton.tsx
@@ -5,7 +5,7 @@ type CircularSkeletonProps = {
 export default function CircularSkeleton({ size }: CircularSkeletonProps) {
   return (
     <div
-      className={`animate-pulse bg-slate-300 rounded-full ${size === "sm" ? "w-8 h-8" : size === "lg" ? "w-32 h-32" : ""}`}
+      className={`animate-pulse bg-slate-300 rounded-full ${size === "sm" ? "w-14 h-14 md:w-8 md:h-8" : size === "lg" ? "w-32 h-32" : ""}`}
     />
   );
 }
