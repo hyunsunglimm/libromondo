96aa216fb318ceaaa7239bc099c012f3ca933d28
hyunsunglimm <gustjd3834@naver.com>
2024-07-17 12:09:26 +0900

    Remove: unnecessary import

--- app/(DefaultLayout)/book/[bookId]/components/ShowMoreBooks.tsx
+++ app/(DefaultLayout)/book/[bookId]/components/ShowMoreBooks.tsx
@@ -2,7 +2,6 @@
 
 import BookCard from "@/components/BookCard";
 import PaginationSection from "@/components/PaginationSection";
-import RootLoading from "@/components/RootLoading";
 import SwiperWrapper from "@/components/SwiperWrapper";
 
 import { BookResponseType } from "@/types/book";
