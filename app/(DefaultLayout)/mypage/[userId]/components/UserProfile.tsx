3ae43584b12731f4b5dc60e3b6e343823bcc9c03
hyunsunglimm <gustjd3834@naver.com>
2024-07-18 21:32:47 +0900

    Refactor: user info update modal change location

--- app/(DefaultLayout)/mypage/[userId]/components/UserProfile.tsx
+++ app/(DefaultLayout)/mypage/[userId]/components/UserProfile.tsx
@@ -78,72 +78,74 @@ export default function UserProfile({ userId }: { userId: string }) {
   };
 
   return (
-    <div className="flex flex-col gap-4 items-center">
-      {user && <ProfileImage image={user.image} name={user.name} size="lg" />}
-      <p className="font-bold text-2xl">{user?.name}</p>
-      {isMe && (
-        <button
-          onClick={() => setIsEdit(true)}
-          className="bg-gray-100 p-2 rounded-md font-bold hover:bg-gray-200 transition"
-        >
-          내 정보 수정
-        </button>
-      )}
-      {!isMe && (
-        <Button
-          onClick={() => toggleFollow(userId, !!isFollow)}
-          className="w-20"
-        >
-          {followLoading ? (
-            <Spinner />
-          ) : (
-            <p>{isFollow ? "언팔로우" : "팔로우"}</p>
-          )}
-        </Button>
-      )}
-      <div className="w-full flex gap-4">
-        <nav className="relative w-full">
+    <>
+      <div className="flex flex-col gap-4 items-center">
+        {user && <ProfileImage image={user.image} name={user.name} size="lg" />}
+        <p className="font-bold text-2xl">{user?.name}</p>
+        {isMe && (
           <button
-            className="border border-black rounded-md p-1 w-full flex justify-between items-center hover:bg-gray-50 trasition"
-            onClick={() => dropdownHandler("following")}
+            onClick={() => setIsEdit(true)}
+            className="bg-gray-100 p-2 rounded-md font-bold hover:bg-gray-200 transition"
           >
-            <div />
-            <p>팔로잉: {user?.following.length}명</p>
-            <DropdownIcon isOpen={dropdownType === "following"} />
+            내 정보 수정
           </button>
-          {dropdownType === "following" && (
-            <ul className="border border-black rounded-md p-4 absolute top-12 w-full bg-white z-10 flex flex-col gap-4 max-h-72 overflow-y-scroll">
-              {user?.following?.map((user) => (
-                <li key={user.id}>
-                  <UserListItem user={user} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </nav>
-        <nav className="relative w-full">
-          <button
-            className="border border-black rounded-md p-1 w-full flex justify-between items-center hover:bg-gray-50 trasition"
-            onClick={() => dropdownHandler("followers")}
+        )}
+        {!isMe && (
+          <Button
+            onClick={() => toggleFollow(userId, !!isFollow)}
+            className="w-20"
           >
-            <div />
-            <p>팔로워: {user?.followers.length}명</p>
-            <DropdownIcon isOpen={dropdownType === "followers"} />
-          </button>
-          {dropdownType === "followers" && (
-            <ul className="border border-black rounded-md p-4 absolute top-12 w-full bg-white z-10 flex flex-col gap-4 max-h-72 overflow-y-scroll">
-              {user?.followers?.map((user) => (
-                <li key={user.id}>
-                  <UserListItem user={user} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </nav>
+            {followLoading ? (
+              <Spinner />
+            ) : (
+              <p>{isFollow ? "언팔로우" : "팔로우"}</p>
+            )}
+          </Button>
+        )}
+        <div className="w-full flex gap-4">
+          <nav className="relative w-full">
+            <button
+              className="border border-black rounded-md p-1 w-full flex justify-between items-center hover:bg-gray-50 trasition"
+              onClick={() => dropdownHandler("following")}
+            >
+              <div />
+              <p>팔로잉: {user?.following.length}명</p>
+              <DropdownIcon isOpen={dropdownType === "following"} />
+            </button>
+            {dropdownType === "following" && (
+              <ul className="border border-black rounded-md p-4 absolute top-12 w-full bg-white z-10 flex flex-col gap-4 max-h-72 overflow-y-scroll">
+                {user?.following?.map((user) => (
+                  <li key={user.id}>
+                    <UserListItem user={user} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </nav>
+          <nav className="relative w-full">
+            <button
+              className="border border-black rounded-md p-1 w-full flex justify-between items-center hover:bg-gray-50 trasition"
+              onClick={() => dropdownHandler("followers")}
+            >
+              <div />
+              <p>팔로워: {user?.followers.length}명</p>
+              <DropdownIcon isOpen={dropdownType === "followers"} />
+            </button>
+            {dropdownType === "followers" && (
+              <ul className="border border-black rounded-md p-4 absolute top-12 w-full bg-white z-10 flex flex-col gap-4 max-h-72 overflow-y-scroll">
+                {user?.followers?.map((user) => (
+                  <li key={user.id}>
+                    <UserListItem user={user} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </nav>
+        </div>
       </div>
       <Modal isOpen={isEdit} onClose={closeModal}>
         <UserInfoEditForm user={user} closeModal={closeModal} />
       </Modal>
-    </div>
+    </>
   );
 }
