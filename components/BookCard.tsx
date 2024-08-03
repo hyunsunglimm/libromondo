f17a5814c571f1933ff33afcf3ce719d39b38f48
hyunsunglimm <gustjd3834@naver.com>
2024-08-04 08:26:24 +0900

    Fix: BookCard image click move link

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -18,14 +18,19 @@ export default function BookCard({ book, index }: BookCardProps) {
 
   return (
     <li className="flex flex-col gap-4 justify-between rounded-sm p-4 w-full border shadow-md">
-      <Image
-        src={book?.thumbnail || "/images/no-image.png"}
-        alt={`${book?.title} 이미지`}
-        width={200}
-        height={300}
-        className="w-full h-72 md:h-52 border rounded-sm"
-        priority={index < 13}
-      />
+      <Link
+        href={`/book/${bookId}`}
+        className="overflow-hidden border rounded-sm"
+      >
+        <Image
+          src={book?.thumbnail || "/images/no-image.png"}
+          alt={`${book?.title} 이미지`}
+          width={200}
+          height={300}
+          className="w-full h-72 md:h-52 hover:scale-110 transition"
+          priority={index < 13}
+        />
+      </Link>
       <div className="flex gap-2">
         <Button asChild className="w-full h-12 md:h-7 text-2xl md:text-base">
           <Link href={`/book/${bookId}`}>상세 보기</Link>
