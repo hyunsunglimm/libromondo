52333902b33bed303aedcc26e41d570450b6cc53
hyunsunglimm <gustjd3834@naver.com>
2024-09-09 15:45:07 +0900

    Feat: apply google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "@/components/GA/GoogleAnalytics";
 import "./globals.css";
 
 import { Inter } from "next/font/google";
@@ -12,6 +13,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        )}
         {children}
       </body>
     </html>
